3ef04f2b1a843fd4a8847d2723d19ecb500cbbae
akshaypawar6066 <99549488+akshaypawar6066@users.noreply.github.com>
2022-02-14 01:13:44 +0530

    Update PasswordCharArray.java

--- PracticeSet/src/PracticeSet/PasswordCharArray.java
+++ PracticeSet/src/PracticeSet/PasswordCharArray.java
@@ -6,7 +6,7 @@ public class PasswordCharArray
 		String s1="bhagwat";
 		
 		System.out.println("Hello");
-
+                System.out.println("Hello Anna ");
 		char[] s2= new char[]{'B','h','a','g','w','t'};
 		System.out.println("String password: " + s1);
 		System.out.println("Character password: " + s2);
